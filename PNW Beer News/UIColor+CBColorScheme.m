6f31451c1632b4d78b3981e11c49e7dd85996c00
Cole Bratcher <colebratcher@gmail.com>
2014-11-05 17:33:40 -0800

    Completed CBColorScheme

--- PNW Beer News/UIColor+CBColorScheme.m
+++ PNW Beer News/UIColor+CBColorScheme.m
@@ -17,5 +17,24 @@
     return [UIColor colorWithRed:0.173 green:0.090 blue:0.098 alpha:1];
 }
 
++ (UIColor *)CBDarkGreenColor
+{
+    return [UIColor colorWithRed:0.000 green:0.278 blue:0.243 alpha:1];
+}
+
++ (UIColor *)CBLightGreenColor
+{
+    return [UIColor colorWithRed:0.000 green:0.478 blue:0.271 alpha:1];
+}
+
++ (UIColor *)CBLimeGreenColor
+{
+    return [UIColor colorWithRed:0.612 green:0.706 blue:0.000 alpha:1];
+}
+
++ (UIColor *)CBGoldenColor
+{
+    return [UIColor colorWithRed:0.992 green:0.741 blue:0.000 alpha:1];
+}
 
 @end
